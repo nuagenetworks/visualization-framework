d3893f8101b73dbf0503d7f2ec3d214740e667fe
Christophe Serafin <christophe.serafin@gmail.com>
2016-09-06 17:51:00 +0200

    Improved drawer theme and created nested items for menu example

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -3,11 +3,27 @@ import { connect } from 'react-redux';
 import { push } from 'redux-router';
 
 import Drawer from 'material-ui/Drawer';
-import MenuItem from 'material-ui/MenuItem';
+import Subheader from 'material-ui/Subheader';
+import {CardHeader} from 'material-ui/Card';
+import { List, ListItem } from 'material-ui/List';
 
 import { Actions, ActionKeyStore } from './redux/actions';
 import {search} from '../utils/elasticsearch';
+import {theme} from '../theme';
 
+var style = {
+    header: {
+        textAlign: 'center',
+    },
+    nestedItems: {
+        background: theme.palette.lightBlue,
+        padding: 0,
+    },
+    nestedItem: {
+        fontSize: "14px",
+        paddingLeft: "16px"
+    }
+}
 
 class MainMenuView extends React.Component {
 
@@ -36,13 +52,62 @@ class MainMenuView extends React.Component {
     render() {
         return (
             <Drawer open={this.props.open} docked={false} onRequestChange={this.props.onRequestChange}>
-                <MenuItem onTouchTap={() => {this.props.goTo('/')}}>Dashboard</MenuItem>
-                <MenuItem onTouchTap={() => {this.props.goTo('/zoom')}}>Zoom</MenuItem>
+                <div style={style.header}>
+                    <p>Visualization Framework</p>
+                    <img src="favicon.ico" alt="icon" role="presentation" width="10%" height="10%" />
+                </div>
 
-                {this.state.domains.map((domain) => {
-                    var domainURI = encodeURIComponent(domain.trim())
-                    return (<MenuItem key={domainURI} onTouchTap={() => {this.props.goTo('/domains/' + domainURI)}}>{domain}</MenuItem>)
-                })}
+                <Subheader>General</Subheader>
+                <List>
+                    <ListItem
+                        primaryText="Dashboard"
+                        onTouchTap={() => {this.props.goTo('/')}}
+                        />
+                    <ListItem
+                        primaryText="Zoom"
+                        onTouchTap={() => {this.props.goTo('/zoom')}}
+                        />
+                </List>
+
+                <Subheader>Enterprises</Subheader>
+                <List>
+                    <ListItem
+                        primaryText="Nokia"
+                        onTouchTap={() => {this.props.goTo('/')}}
+                        initiallyOpen={true}
+                        primaryTogglesNestedList={false}
+                        nestedItems={[
+                            <div style={style.nestedItems}>
+                                <Subheader>Domains</Subheader>
+                                    {this.state.domains.map((domain) => {
+                                        var domainURI = encodeURIComponent(domain.trim())
+                                        return (<ListItem
+                                                    key={domainURI}
+                                                    primaryText={domain}
+                                                    style={style.nestedItem}
+                                                    onTouchTap={() => {this.props.goTo('/domains/' + domainURI)}}
+                                                    />)
+                                    })}
+
+                                <Subheader>NSGs</Subheader>
+                                <ListItem
+                                    key={1}
+                                    style={style.nestedItem}
+                                    primaryText="NSG 1"
+                                    />
+                                <ListItem
+                                    key={2}
+                                    style={style.nestedItem}
+                                    primaryText="NSG 2"
+                                    />
+                                <ListItem
+                                    key={3}
+                                    style={style.nestedItem}
+                                    primaryText="NSG 3"
+                                    />
+                            </div>
+                            ]}/>
+                </List>
             </Drawer>
         );
     }
--- src/theme/index.js
+++ src/theme/index.js
@@ -2,14 +2,37 @@ import lightMuiTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 export const lightTheme = getMuiTheme(lightMuiTheme, {
-  palette: {
-    primary1Color: '#34699b',
-    primary2Color: '#83a8cd',
-    primary3Color: '#3b5874',
-    thinBorder: 'solid 1px',
-    boldBorder: 'solid 2px',
-    smallBorderRadius: '2px',
-  }
+    palette: {
+        // Material-UI
+        primary1Color: '#ffffff',
+        primary2Color: '#83a8cd',
+        primary3Color: '#3b5874',
+        textColor: '#ffffff',
+        alternateTextColor: '#ffffff',
+        accent1Color: '#E7808B', //red
+        accent2Color: '#D1D1D1', // gray
+        accent3Color: '#DEBB4E', // orange
+
+        canvasColor: '#E7808B', //red
+        borderColor: '#D1D1D1', // gray or #797979
+        disabledColor: '#DEBB4E', // orange
+        pickerHeaderColor: '#4E95D8', // blue
+        clockCircleColor: '#55C6C9', // light blue
+        shadowColor: '#56B79B', // Green
+
+        // Custom
+        thinBorder: 'solid 1px',
+        boldBorder: 'solid 2px',
+        transparentColor: 'transparent',
+        containerBackgroundColor: '#3F5574',
+        lightBlue: '#506B93',
+    },
+    drawer: {
+        color: '#3F5574',
+    },
+    appBar: {
+        color: '#3F5574',
+    },
 });
 
 export const theme = lightTheme;
