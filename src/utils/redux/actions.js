e2a82cb64933085931d00df41e0c0fdcc4eab142
curran <curran.kelleher@gmail.com>
2016-09-12 13:57:08 +0530

    Move ElasticSearch logic into thunk action creator

--- src/utils/redux/actions.js
+++ src/utils/redux/actions.js
@@ -1,3 +1,5 @@
+import {search} from '../elasticsearch';
+
 export const ActionTypes = {
     ES_SEARCH_REQUEST: "ES_SEARCH_REQUEST",
     ES_SEARCH_SUCCESS: "ES_SEARCH_SUCCESS"
@@ -9,6 +11,27 @@ export const ActionKeyStore = {
 };
 
 export const Actions = {
+
+    // This is a thunk action creator that will
+    // initiate a request to ElasticSearch.
+    fetchSearch: function (){
+        return function (dispatch){
+            dispatch(Actions.searchRequest);
+
+            search({
+                q: 'domains',
+            }).then(function () {
+                let results = ['domain 1', 'domain 2', 'domain 3'];
+                dispatch(Actions.searchResponse(results));
+                console.log('ElasticSearch is UP !');
+
+            }, function () {
+                dispatch(Actions.searchResponse([]));
+                console.error('ElasticSearch cluster is down!');
+            });
+        }
+    },
+
     searchRequest: function() {
         return {
             type: ActionTypes.ES_SEARCH_REQUEST
