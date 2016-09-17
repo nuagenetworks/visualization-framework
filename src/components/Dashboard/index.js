7c416f29f6091d49f801741d344a58ae25b8e946
curran <curran.kelleher@gmail.com>
2016-09-17 14:56:30 +0530

    Indentation in Dashboard component

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -38,9 +38,24 @@ class DashboardView extends React.Component {
 
 
 const mapStateToProps = (state, ownProps) => ({
-    configuration: state.dashboards.getIn([DashboardActionKeyStore.KEY_STORE_DASHBOARDS, ownProps.params.dashboardID, DashboardActionKeyStore.KEY_STORE_DATA]),
-    fetching: state.dashboards.getIn([DashboardActionKeyStore.KEY_STORE_DASHBOARDS, ownProps.params.dashboardID, DashboardActionKeyStore.KEY_STORE_IS_FETCHING]),
-    error: state.dashboards.getIn([DashboardActionKeyStore.KEY_STORE_DASHBOARDS, ownProps.params.dashboardID, DashboardActionKeyStore.KEY_STORE_ERROR]),
+
+    configuration: state.dashboards.getIn([
+        DashboardActionKeyStore.KEY_STORE_DASHBOARDS,
+        ownProps.params.dashboardID,
+        DashboardActionKeyStore.KEY_STORE_DATA
+    ]),
+
+    fetching: state.dashboards.getIn([
+        DashboardActionKeyStore.KEY_STORE_DASHBOARDS,
+        ownProps.params.dashboardID,
+        DashboardActionKeyStore.KEY_STORE_IS_FETCHING
+    ]),
+
+    error: state.dashboards.getIn([
+        DashboardActionKeyStore.KEY_STORE_DASHBOARDS,
+        ownProps.params.dashboardID,
+        DashboardActionKeyStore.KEY_STORE_ERROR
+    ])
 });
 
 
