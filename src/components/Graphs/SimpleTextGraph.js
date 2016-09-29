33f95d41baf56806ed5eba06721a1f03147448f6
Christophe Serafin <christophe.serafin@gmail.com>
2016-09-29 13:35:10 +0200

    Updated SimpleTextGraph to wait for its data in the response property

--- src/components/Graphs/SimpleTextGraph.js
+++ src/components/Graphs/SimpleTextGraph.js
@@ -1,23 +1,44 @@
 import React from "react";
 
+import CircularProgress from "material-ui/CircularProgress";
 
 /*
     This is a very basic graph that displays a text message
 */
 class SimpleTextGraph extends React.Component {
     render() {
-        const { text } = this.props;
+        const { response, queryConfiguration } = this.props;
+        let body;
+
+        if (response && !response.isFetching) {
+            body = (
+                <div>
+                    {response.results.length}
+                    <br />
+                    {queryConfiguration.title}
+                </div>
+            );
+        }
+        else {
+
+            body = (
+                <div>
+                    <CircularProgress color="#eeeeee" /> Loading graph
+                </div>
+            );
+        }
 
         return (
             <div>
-                { text }
+                { body }
             </div>
         );
     }
 }
 
 SimpleTextGraph.propTypes = {
-  text: React.PropTypes.string
+  title: React.PropTypes.string,
+  response: React.PropTypes.object
 };
 
 export default SimpleTextGraph;
