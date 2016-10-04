eff2f0ec38a99617c596ace4a101cf19d824d016
Christophe Serafin <christophe.serafin@gmail.com>
2016-10-04 19:03:08 +0200

    Removed favicon and added Nuage Networks logo

---
+++ src/components/App/styles.js
@@ -0,0 +1,27 @@
+import { theme } from "../../theme";
+
+const Styles = {
+    header: {
+        textAlign: "center",
+        color: "#ffffff",
+        fontSize: "120%",
+        padding: "10px",
+    },
+    nestedItems: {
+        background: theme.palette.lightBlue,
+        padding: 0,
+    },
+    nestedItem: {
+        fontSize: "14px",
+        paddingLeft: "16px",
+        color: "#ffffff",
+    },
+    listItem: {
+        color: "#ffffff",
+    },
+    subHeader: {
+        color: "#ffffff",
+    }
+}
+
+export default Styles;
