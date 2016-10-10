4f680b75bd4b51f7d9df08e9ebe8a096ad1d9816
curran <curran.kelleher@gmail.com>
2016-10-10 18:18:52 +0530

    Got first table working

--- src/components/Graphs/Table.js
+++ src/components/Graphs/Table.js
@@ -3,18 +3,20 @@ import tabify from "../../utils/tabify";
 
 export default class Table extends React.Component {
     render() {
-        const { response } = this.props;
+        const { response, configuration } = this.props;
         const data = tabify(response.results);
+        const properties = configuration.data;
+        const columns = properties.columns;
 
-        // TODO make column order part of the configuration.
-        const columns = Object.keys(data);
+        console.log(data);
+        console.log(columns);
 
         return (
             <table className="table table-bordered">
                 <thead>
                     <tr>
-                        { columns.map((column) =>(
-                            <th>{column}</th>
+                        { columns.map(({column, label}, i) =>(
+                            <th key={i}>{ label || column }</th>
                         )) }
                     </tr>
                 </thead>
