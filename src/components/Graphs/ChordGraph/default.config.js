ad94b3cf2d9f6c80315756c2036e88fff059143e
curran <curran.kelleher@gmail.com>
2016-10-26 23:30:36 +0530

    Use original colors in default configuration. Closes #131

--- src/components/Graphs/ChordGraph/default.config.js
+++ src/components/Graphs/ChordGraph/default.config.js
@@ -8,13 +8,5 @@ export const properties = {
     transitionDuration: 500,
     defaultOpacity: 0.6,
     fadedOpacity: 0.1,
-    colors: [
-        theme.palette.yellowLightColor,
-        theme.palette.orangeLightColor,
-        theme.palette.blueLightColor,
-        theme.palette.pinkLightColor,
-        theme.palette.greenColor,
-        theme.palette.yellowDarkColor,
-        theme.palette.orangeLighterColor
-    ]
+    colors: ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf", "#aec7e8", "#ffbb78", "#98df8a", "#ff9896", "#c5b0d5", "#c49c94", "#f7b6d2", "#c7c7c7", "#dbdb8d", "#9edae5"]
 }
