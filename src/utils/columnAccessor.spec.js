4bfb7f5c8e80b9c7009ac388f55843aec7dc8f12
curran <curran.kelleher@gmail.com>
2016-11-18 16:09:00 +0530

    Add test for number formatter

--- src/utils/columnAccessor.spec.js
+++ src/utils/columnAccessor.spec.js
@@ -1,9 +1,14 @@
 import columnAccessor from './columnAccessor';
 
 describe('Util:columnAccessor', () => {
-    it('should run this test', () => {
+    it('should access the value of a column', () => {
         const accessor = columnAccessor({ column: 'foo' });
         const d = { foo: 5 };
         expect(accessor(d)).toEqual(5);
+    });
+    it('should apply a number formatter', () => {
+        const accessor = columnAccessor({ column: 'foo', format: '.0s' });
+        const d = { foo: 10000 };
+        expect(accessor(d)).toEqual('10k');
     });
 });
