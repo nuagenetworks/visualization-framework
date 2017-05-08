fce2fa463f7c906ad54378d27e878b39127b1a16
nxanil <anil@nodexperts.com>
2017-05-08 18:11:04 +0530

    Comments and Indentations

--- server/app/server.js
+++ server/app/server.js
@@ -10,31 +10,23 @@ import Constants from './config/constants';
 
 const app = express();
 
-// Helmet helps you secure your Express apps by setting various HTTP headers
-// https://github.com/helmetjs/helmet
+// Securing express APPs
 app.use(helmet());
 
-// Enable CORS with various options
-// https://github.com/expressjs/cors
+// Enabling CORS
 app.use(cors());
 
 // Request logger
-// https://github.com/expressjs/morgan
 if (!Constants.envs.test) {
   app.use(morgan('dev'));
 }
 
 // Parse incoming request bodies
-// https://github.com/expressjs/body-parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Lets you use HTTP verbs such as PUT or DELETE
-// https://github.com/expressjs/method-override
 app.use(methodOverride());
-
-// Mount public routes
-app.use('/public', express.static(`${__dirname}/public`));
 
 // Mount API routes
 app.use(Constants.apiPrefix, routes);
