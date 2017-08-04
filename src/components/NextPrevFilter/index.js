0bc9ae00b5eaa1fe129e5b1ece7c874f3070143d
nxanil <anil@nodeexperts.com>
2017-08-04 19:05:47 +0530

    Resolved issue for scroll bar buttons #419

--- src/components/NextPrevFilter/index.js
+++ src/components/NextPrevFilter/index.js
@@ -25,25 +25,31 @@ export class NextPrevFilter extends React.Component {
             filterOptions: {
                 "startTime": "startTime",
                 "endTime": "endTime",
-                "prevStartTime": "prevStartTime"
+                "prevStartTime": "prevStartTime",
+                "page": "page"
             }
         }
     }
 
     componentWillMount() {
+
+        if(this.props.context[`${this.props.visualizationId}-${this.state.filterOptions.page}`]) {
+            this.setState({page: this.props.context[`${this.props.visualizationId}-${this.state.filterOptions.page}`]});
+        }
         this.updateContextValue(this.props);
     }
 
     componentWillReceiveProps(nextProps) {
-        if(this.props.context.interval !== nextProps.context.interval) {
-            this.setState({page : 1})
 
-            let prevStartTime = (nextProps.context.prevStartTime) ? nextProps.context.prevStartTime : 0;
+        if(this.props.context.interval !== nextProps.context.interval) {
+            this.setState({page: 1});
             let finalContext = Object.assign({}, nextProps.context, {
                 [`${nextProps.visualizationId}-${this.state.filterOptions.startTime}`] : nextProps.context.startTime,
                 [`${nextProps.visualizationId}-${this.state.filterOptions.endTime}`] : 'now',
-                [`${nextProps.visualizationId}-${this.state.filterOptions.prevStartTime}`]: prevStartTime
+                [`${nextProps.visualizationId}-${this.state.filterOptions.prevStartTime}`]: (nextProps.context.prevStartTime) ? nextProps.context.prevStartTime : 0,
+                [`${nextProps.visualizationId}-${this.state.filterOptions.page}`] : 1,
             });
+
             this.props.goTo(window.location.pathname, finalContext);
         }
         this.updateContextValue(nextProps);
@@ -111,7 +117,8 @@ export class NextPrevFilter extends React.Component {
         let queryParams = Object.assign({}, context, {
                 [`${visualizationId}-${this.state.filterOptions.startTime}`]: startTime + (this.state.unit),
                 [`${visualizationId}-${this.state.filterOptions.endTime}`]: endTime + (this.state.unit),
-                [`${visualizationId}-${this.state.filterOptions.prevStartTime}`]: prevStartTime + (this.state.unit)
+                [`${visualizationId}-${this.state.filterOptions.prevStartTime}`]: prevStartTime + (this.state.unit),
+                [`${visualizationId}-${this.state.filterOptions.page}`] : this.state.page - 1,
             });
 
         this.props.goTo(window.location.pathname, queryParams);
@@ -135,7 +142,8 @@ export class NextPrevFilter extends React.Component {
         let queryParams = Object.assign({}, context, {
                 [`${visualizationId}-${this.state.filterOptions.startTime}`]: startTime + (this.state.unit),
                 [`${visualizationId}-${this.state.filterOptions.endTime}`]: endTime + (this.state.unit),
-                [`${visualizationId}-${this.state.filterOptions.prevStartTime}`]: prevStartTime + (this.state.unit)
+                [`${visualizationId}-${this.state.filterOptions.prevStartTime}`]: prevStartTime + (this.state.unit),
+                [`${visualizationId}-${this.state.filterOptions.page}`] : this.state.page + 1,
             });
 
         this.props.goTo(window.location.pathname, queryParams);
--- src/components/Visualization/index.js
+++ src/components/Visualization/index.js
@@ -482,6 +482,7 @@ class VisualizationView extends React.Component {
                 { this.renderFiltersToolBar() }
 
                 { this.renderSharingOptions() }
+                <div className="clearfix"></div>
                 <CardText style={cardText}>
                     { this.renderVisualizationIfNeeded() }
                     {description}
@@ -534,10 +535,12 @@ const mapStateToProps = (state, ownProps) => {
     let context = {};
     for (let key in orgContexts) {
       if(orgContexts.hasOwnProperty(key)) {
-        context[key.replace(`${configurationID}-`, '')] = orgContexts[key];
+        let filteredKey = key.replace(`${configurationID}-`, '');
+        if(!context[filteredKey] || key.includes(`${configurationID}-`))
+            context[filteredKey] = orgContexts[key];
       }
     }
-
+    
     const props = {
         id: configurationID,
         context: context,
