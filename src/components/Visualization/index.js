a0553b91cba027168cacd39cf1353411b6de8443
nxanil <anil@nodeexperts.com>
2017-09-27 14:02:04 +0530

    conversion

--- src/components/Visualization/index.js
+++ src/components/Visualization/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import ReactInterval from 'react-interval';
-import safeEval from "safe-eval"
+import safeEval from "cross-safe-eval"
 
 import $ from "jquery";
 import CopyToClipboard from 'react-copy-to-clipboard';
@@ -287,6 +287,15 @@ class VisualizationView extends React.Component {
         }
 
         const data = ServiceManager.tabify(queryConfiguration, response.results);
+
+        // const data = [{
+        //     "key_as_string": "2017-07-05T00:00:00.000Z",
+        //     "ts": 1499212800000,
+        //     "doc_count": 144,
+        //     "MEMORY": 49.06944444444444,
+        //     "CPU": 60.958333333333336,
+        //     "DISK": 0.09722222222222
+        // }];
 
         if (!data || !data.length) {
             return this.renderCardWithInfo("No data to visualize", "bar-chart");
