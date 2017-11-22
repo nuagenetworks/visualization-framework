b4d9fd681e59bd143357722f82d525d0e083c089
nxanil <anil@nodexperts.com>
2017-11-22 12:42:14 +0530

    Removed unused configs.

--- server/app/configurations/constants.js
+++ server/app/configurations/constants.js
@@ -5,23 +5,23 @@ import readConfig from 'read-config'
 const config = readConfig(path.resolve(__dirname, '../../config.json'));
 
 const defaultConfig = {
-  env: process.env.NODE_ENV,
+  env: config.NODE_ENV,
   get envs() {
     return {
-      test: process.env.NODE_ENV === 'test',
-      development: process.env.NODE_ENV === 'development',
-      production: process.env.NODE_ENV === 'production',
+      test: config.NODE_ENV === 'test',
+      development: config.NODE_ENV === 'development',
+      production: config.NODE_ENV === 'production',
     };
   },
-  port: config.APP_SERVER_PORT || 8010,
-  ip: config.APP_SERVER_IP || 'localhost',
+  port: config.MIDDLEWARE_SERVER_PORT || 8010,
+  ip: config.MIDDLEWARE_SERVER_IP || 'localhost',
   release: require('../../package.json').version,
   baseDir: path.normalize(__dirname + '/../..'),
   apiPrefix: config.APP_PREFIX || '/middleware/api',
   cert: config.HTTPS_CERT_PATH || null,
   key: config.HTTPS_KEY_PATH || null,
   elasticSearchHost: config.APP_ELASTICSEARCH_HOST || null,
-  vsdApiEndPoint: config.APP_VSD_API_ENDPOINT || null
+  vsdApiEndPoint: config.REACT_APP_VSD_API_ENDPOINT || null
 }
 
 export default defaultConfig;
--- server/index.js
+++ server/index.js
@@ -1,5 +1,3 @@
-// Used as entry for development server only
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 require('babel-register');
 require('./app');
