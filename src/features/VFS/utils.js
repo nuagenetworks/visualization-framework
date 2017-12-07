2fb5f1888ac6eec9fb4f4396a79ea5322ecf98f1
Natalia Balus <natalia.balus@nokia.com>
2017-12-07 14:50:03 -0800

    Refactoring

--- src/features/VFS/utils.js
+++ src/features/VFS/utils.js
@@ -1,9 +1,4 @@
-import React from 'react';
-import { TwoColumnRow } from '../components';
-
 export const getMetaDataAttribute = (data, attrName) => data && data.hasOwnProperty("nuage_metadata") ? data.nuage_metadata[attrName] : null;
-
-export const twoColumnRow = (col1, col2) => <TwoColumnRow firstColumnProps={col1} secondColumnProps={col2} />
 
 export const buildOptions = (options) => {
     if (options && options.data && options.data.length > 0) {
@@ -16,4 +11,63 @@ export const buildOptions = (options) => {
         return options.error;
     }
     return "No Data available";
+}
+
+export const getNetworkItems = (type, props) => {
+    const {
+        zones,
+        subnets,
+        policygroups,
+        pgexpressions,
+        enterprisenetworks,
+        networkmacrogroups,
+        l2domains,
+        mirrordestinations,
+        overlaymirrordestinations,
+        l7applicationsignatures,
+        vfrules,
+    } = props;
+    switch (type) {
+        case 'ZONE':
+            return zones;
+        case 'SUBNET':
+            return subnets;
+        case 'POLICYGROUP':
+            return policygroups;
+        case 'PGEXPRESSION':
+            return pgexpressions;
+        case 'ENTERPRISE_NETWORK':
+            return enterprisenetworks;
+        case 'NETWORK_MACRO_GROUP':
+            return networkmacrogroups;
+        case 'l2domainID':
+            return l2domains;
+        case 'mirrorDestinationID':
+            return mirrordestinations;
+        case 'overlayMirrorDestinationID':
+            return overlaymirrordestinations;
+        case 'associatedL7ApplicationSignatureID':
+            return l7applicationsignatures;
+        case 'virtualfirewallrules':
+            return vfrules;
+        default:
+            return null;
+    }
+}
+
+export const  getEnterpriseID = (props) => {
+    const {
+        data,
+        query,
+        context
+    } = props;
+
+    let enterpriseID = getMetaDataAttribute(data, 'enterpriseId');
+    if (!enterpriseID && query) {
+        enterpriseID = query.enterpriseID;
+    }
+    if (!enterpriseID && context) {
+        enterpriseID = context.enterpriseID ? context.enterpriseID : context.enterpriseId;
+    }
+    return enterpriseID;
 }
