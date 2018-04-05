5e17e07200655376366d62ded0fcbd72fcdaaf36
bmukheja <bharat.mukheja@gmail.com>
2018-04-05 18:54:58 -0400

    Temp added google api key

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 // server.js
 var express = require("express");
 var path = require("path");
-require('dotenv').load();
+//The below code snippet is only before the time the VSD starts sending api key.
+//require('dotenv').load();
+process.env['REACT_APP_ELASTICSEARCH_HOST'] = 'http://localhost:9200';
+process.env['REACT_APP_VSD_API_ENDPOINT'] = 'https://vsd.com:8443/nuage/api/';
+process.env['REACT_APP_GOOGLE_MAP_LAT'] = 37.09024;
+process.env['REACT_APP_GOOGLE_MAP_LNG'] = 95.712891;
+process.env['REACT_APP_GOOGLE_MAP_ZOOM'] = 4;
+process.env['REACT_APP_GOOGLE_MAP_API'] = 'AIzaSyD6gAsFkFAfe0nTGYPUvZuMsmuIxPXnHNI';
+process.env['REACT_APP_GOOGLE_MAP_LIBRARIES'] = 'geometry,drawing,places'
+//
+
 var app = express();
 
 var homepage = "/reports"; // Quickly edit this line to test the build
