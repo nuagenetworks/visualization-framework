b70123513e029ef07ae77097ccf73451021cb6c0
Natalia Balus <nataba@gmail.com>
2018-10-02 02:45:59 -0700

    Fix build issues, PROD-8398

--- src/components/Editor/ModalEditor.js
+++ src/components/Editor/ModalEditor.js
@@ -4,7 +4,7 @@ import { FlatButton } from 'material-ui';
 import { formSubmit, clickSubmit } from './actions';
 import style from './style';
 import { Modal } from '../../ui-components';
-import { buttonFlat } from '../../ui-components/style';
+import componentStyle  from '../../ui-components/style';
 import Editor from '.';
 
 export const ModalEditor = props => {
@@ -20,6 +20,7 @@ export const ModalEditor = props => {
         errored,
         onDone,
     } = props;
+    const buttonFlat = componentStyle.buttonFlat;
     let buttons = [
         <FlatButton
             primary={true}
