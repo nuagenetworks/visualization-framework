86bbe870f4fabe3ac271e8224ad2a6dbeffeeef4
Bharat Mukheja <bharat.mukheja@gmail.com>
2019-02-14 17:32:46 -0800

    Added rest of the areas, modified heatmap to 14 days, modified variationtextgraph to simpletextgraph

--- src/configs/nuage/elasticsearch/tabification/index.js
+++ src/configs/nuage/elasticsearch/tabification/index.js
@@ -1,3 +1,4 @@
 export { default as top20TalkerAARDomain } from './top20TalkerAARDomain';
 export { default as infraRegressionCount } from './infraRegressionCount';
 export { default as testSuitePerformanceCounts } from './testSuitePerformanceCounts';
+export { default as aqlSimpleText } from './aqlSimpleText';
