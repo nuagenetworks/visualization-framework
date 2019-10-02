f75b278bb0b0cb0c35a93eead37613586280643c
Bharat Mukheja <bharat.mukheja@gmail.com>
2019-10-02 09:07:30 -0700

    Changes to Infra Analytics dashboard - 1. Corrected the data of all the dashboards

--- src/configs/nuage/elasticsearch/tabification/index.js
+++ src/configs/nuage/elasticsearch/tabification/index.js
@@ -2,6 +2,8 @@ export { default as top20TalkerAARDomain } from './top20TalkerAARDomain';
 export { default as infraRegressionCount } from './infraRegressionCount';
 export { default as testSuitePerformanceCounts } from './testSuitePerformanceCounts';
 export { default as aqlSimpleText } from './aqlSimpleText';
+export { default as infraPieGraph } from './infraPieGraph';
+export { default as infraStackedBar } from './infraStackedBar';
 export { default as weekendCoverage } from './weekendCoverage';
 export { default as regressExtractRunLevel } from './regressExtractRunLevel';
 export { default as defaultTabify } from './defaultTabify';
