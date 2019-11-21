ac076cf6f3cd4bb0c81738344870bf931d64c84f
Bharat Mukheja <bharat.mukheja@nokia.com>
2019-11-21 11:07:28 -0800

    Fixed a few bugs in expressheatmap tabification

--- src/configs/nuage/elasticsearch/tabification/expressHeatmap.js
+++ src/configs/nuage/elasticsearch/tabification/expressHeatmap.js
@@ -1,22 +1,24 @@
-import { stringify } from "querystring";
-import { readJsonFile } from "./common";
-
+import objectPath from 'object-path';
+import evalExpression from 'eval-expression';
+import _ from 'lodash';
 /*
   This utility will convert the nested data structure
   returned from an ElasticSearch query into a tabular
   data structure represented as an array of row objects.
+
+  Inspired by Kibana's implementation, found at
+  https://github.com/elastic/kibana/blob/master/src/ui/public/agg_response/tabify/tabify.js
 */
-
-
-export default function tabify(response, query = {}) {
+export default function expressHeatmap(response, query = {}) {
     let table;
 
     if (response.aggregations) {
         const tree = collectBucket(response.aggregations);
         table = flatten(tree);
-
     } else if (response.hits) {
-        table = response.hits.hits.map((d) => d._source);
+        table = response.hits.hits.map((d) => {
+            return d._source
+        });
 
     } else if (Array.isArray(response)) {
         table = response;
@@ -24,12 +26,15 @@ export default function tabify(response, query = {}) {
     } else {
         throw new Error("Tabify() invoked with invalid result set. Result set must have either 'aggregations' or 'hits' defined.");
     }
+    
     let express_suites;
     if (query.tabifyOptions.express_suites){
         express_suites = query.tabifyOptions.express_suites;
     }
     
     table = processForHeatmap(table, express_suites);
+
+    table = flatArray(table)
 
     if (process.env.NODE_ENV === "development") {
         console.log("Results from tabify (first 3 rows only):");
@@ -55,8 +60,8 @@ function processForHeatmap(table, suites){
     for (let i=0; i<table.length; i++){
         item = table[i];
         if (typeof item.testsuite == 'string') {
-        item.result = result_codes[item.result];
-        suites_in_response.add(item.testsuite);
+            item.result = result_codes[item.result];
+            suites_in_response.add(item.testsuite);
         }
     }
     let suites_left = suites.filter((suite) => { return !suites_in_response.has(suite);});
@@ -72,17 +77,84 @@ function processForHeatmap(table, suites){
     return table;
 }
 
+function findPropertyPath(obj, name) {
+    for (var prop in obj) {
+        if (prop === name) {
+            return name;
+        } else if (typeof obj[prop] === "object") {
+            var result = findPropertyPath(obj[prop], name);
+            if (result) { return prop + '.' + result; }
+        }
+    }
+
+    return null;
+}
+
+function flatArray(data) {
+
+  let finalData = [];
+  data.forEach(item => {
+      let result = cartesianProduct(item);
+      finalData = [...finalData, ...result];
+  })
+
+  return finalData;
+}
+
+
+function cartesianProduct(data) {
+    let final = [];
+    let arrayExists = false;
+
+    let keys = Object.keys(data);
+    for(let i = 0; i < keys.length; i++) {
+        if(Array.isArray(data[keys[i]])) {
+            if (data[keys[i]].length === 0) {
+                data[keys[i]].push({});
+            }
+
+            data[keys[i]].forEach(item => {
+                final.push({...data, [keys[i]]: item})
+            });
+            arrayExists = true;
+            break;
+        } else if (data[keys[i]] && typeof data[keys[i]] === 'object') {
+
+            let products = cartesianProduct(data[keys[i]]);
+            if(products.length > 1) {
+                products.forEach(item => {
+                    final.push({...data, [keys[i]]: item})
+                });
+                arrayExists = true;
+                break;
+            } else if (products.length === 1) {
+                data[keys[i]] = products[0];
+            }
+        }
+    };
+
+    if(arrayExists) {
+        final = flatArray(final);
+    } else {
+        final.push(data);
+    }
+
+    return final;
+}
+
 function collectBucket(node, stack=[]) {
     if (!node)
         return;
-    
+
     const keys = Object.keys(node);
-    
+
     // Use old school `for` so we can break control flow by returning.
     for(let i = 0; i < keys.length; i++) {
         const key = keys[i];
         const value = node[key];
-        if (typeof value === 'object' && value !== null) {
+
+        if (typeof value === 'object') {
+
             if ("hits" in value && Array.isArray(value.hits) && value.hits.length === 1) {
                 if ("sort" in value.hits[0]) {
                     value.hits[0]._source['sort'] = value.hits[0].sort[0];
@@ -99,13 +171,7 @@ function collectBucket(node, stack=[]) {
             {
                 return extractBuckets(value, [...stack, key]);
             }
-
             return collectBucket(value, [...stack, key]);
-        }
-
-        if (key === "value" && typeof value !== "object" && stack.length === 1) {
-            let collectedObject = collectBucket({[stack[0]]: value});
-            node = collectedObject;
         }
     }
 
@@ -148,9 +214,15 @@ function extractTree(buckets, stack) {
             if(key === "key"){
                 key = stack[stack.length - 2]
             }
-            
+
+            if (typeof value === 'object') {
+                if("value" in value){
+                    value = value.value;
+                }
+            }
+
             tree[key] = value;
-        
+
             return tree;
         }, {});
     });
@@ -177,12 +249,22 @@ function flatten(tree, parentNode={}){
                 .map((key) => {
                     const value = node[key];
                     if (Array.isArray(value)) {
-                        return value;
+
+                        if(value.length) {
+                            return value.map(item => {
+                                if (item[key]) {
+                                    return item;
+                                }
+
+                                return {[key]: item};
+                            });
+                        }
+
+                        node[key] = null;
                     }
                     return false;
                 })
                 .filter((d) => d);
-
             switch (childTrees.length) {
 
                 // Leaf node case, return the node.
@@ -190,14 +272,14 @@ function flatten(tree, parentNode={}){
                     return node;
 
                 // Non-leaf node case, recurse on the child nodes.
-                case 1:
+                default:
                     const childTree = childTrees[0];
                     if(childTree.length === 0){
                         return node;
                     }
                     return flatten(childTree, node);
-                default:
-                    throw new Error("This case should never happen");
+                // default:
+                //     throw new Error("This case should never happen");
             }
         })
 
